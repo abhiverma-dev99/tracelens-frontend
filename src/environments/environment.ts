bf528d322c506af687e96ead0df11659b39a2ae6
abhiverma-dev99 <abhiverma.dev99@gmail.com>
2026-09-05 23:57:16 +0530

    Switch to Render backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'https://tracelens-0dakofo6.b4a.run/api',
+  apiUrl: 'https://tracelens-7hdm.onrender.com/api',
   ingestKey: '80df8997-8564-4018-b818-b5a67fe26d61', 
   projectName: 'TraceLens Frontend'
 };
