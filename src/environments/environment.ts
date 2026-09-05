8793d31ec813d0d0a112c6d06a9f983bfbf159ad
abhiverma-dev99 <abhiverma.dev99@gmail.com>
2026-09-05 20:31:32 +0530

    updated backend api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://tracelens-0dakofo6.b4a.run/api',
   ingestKey: '80df8997-8564-4018-b818-b5a67fe26d61', 
   projectName: 'TraceLens Frontend'
 };
